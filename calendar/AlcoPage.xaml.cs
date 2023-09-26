d843b8e3616ac5eb050828cbbe375d97e7b54f7c
MPTkzl <105377542+yye1337@users.noreply.github.com>
2023-09-26 18:01:13 +0300

    123

---
+++ calendar/AlcoPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calendar
+{
+    /// <summary>
+    /// Логика взаимодействия для AlcoPage.xaml
+    /// </summary>
+    public partial class AlcoPage : Page
+    {
+        public AlcoPage()
+        {
+            InitializeComponent();
+
+            Alco beer = new Alco();
+            beer.ImageAlco.Source = new BitmapImage(new Uri("/image/beer.png", UriKind.Relative));
+            beer.NameAlco.Text = "Пиво";
+            Alco wine = new Alco();
+            wine.ImageAlco.Source = new BitmapImage(new Uri("/image/wine.png", UriKind.Relative));
+            wine.NameAlco.Text = "Вино";
+            Alco cocktail = new Alco();
+            cocktail.ImageAlco.Source = new BitmapImage(new Uri("/image/cocktail.png", UriKind.Relative));
+            cocktail.NameAlco.Text = "Коктейль";
+            Alco whiskey = new Alco();
+            whiskey.ImageAlco.Source = new BitmapImage(new Uri("/image/whickey.png", UriKind.Relative));
+            whiskey.NameAlco.Text = "Виски";
+
+            List<Alco> alcos = new List<Alco>() { beer, wine, cocktail, whiskey };
+            AlcoList.ItemsSource = alcos;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new CalendarPage();
+        }
+    }
+}
---
+++ calendar/CalendarPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calendar
+{
+    /// <summary>
+    /// Логика взаимодействия для CalendarPage.xaml
+    /// </summary>
+    public partial class CalendarPage : Page
+    {
+        public CalendarPage()
+        {
+            InitializeComponent();
+            Calendar.SelectedDate = DateTime.Now;
+            Panel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Calendar.SelectedDate = ((DateTime)Calendar.SelectedDate).AddMonths(+1);
+            Panel();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Calendar.SelectedDate = ((DateTime)Calendar.SelectedDate).AddMonths(-1);
+            Panel();
+        }
+
+        public void Panel()
+        {
+            Day.Children.Clear();
+            
+            for (int i = 1; i <= DateTime.DaysInMonth(((DateTime)Calendar.SelectedDate).Year, ((DateTime)Calendar.SelectedDate).Month); i++)
+            {
+                DayPanel day = new DayPanel();
+                day.DataName.Content = i.ToString();
+                day.DataName.Click += DayName_Click;
+                Day.Children.Add(day);
+            }
+        }
+
+        private void DayName_Click(object sender, RoutedEventArgs e)
+        {
+            FrameCheck.Content = new AlcoPage();
+        }
+    }
+}
